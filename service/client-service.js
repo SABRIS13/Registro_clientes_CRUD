ca73a233688125026bb1184a0e3d87ad51643f4f
Sabrina Contreras <sabriscon13@hotmail.com>
2023-02-09 12:07:17 -0700

    Crear y eliminar clientes: registro, funciones de post y delete

--- service/client-service.js
+++ service/client-service.js
@@ -1,41 +1,29 @@
-const crearNuevaLinea = (nombre,email) =>{
-    const linea=document.createElement("tr");
-    const content=`
-    <td class="td" data-td>${nombre}</td>
-    <td>${email}</td>
-    <td>
-      <ul class="table__button-control">
-        <li>
-          <a href="../screens/editar_cliente.html" class="simple-button simple-button--edit">Editar</a>
-        </li>
-        <li>
-          <button class="simple-button simple-button--delete" type="button">
-            Eliminar
-          </button>
-        </li>
-      </ul>
-    </td>`;
-    linea.innerHTML=content;
-    return linea;
-};
-
-const table=document.querySelector("[data-table]");
-
 
 //Abrir http(método,url())
 //CRUD=  Create ( POST) Read (GET) Update (PUT/PATCH)  Delete (DELETE)
 
+//Comunicación con servidor
+const listaClientes = () =>
+  fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json());
 
-const listaClientes= ()=>fetch("http://localhost:3000/perfiles").then(respuesta=>
-    respuesta.json()
-    );
+const crearCliente = (nombre, email) => {
+  return fetch("http://localhost:3000/perfil", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ nombre, email, id: uuid.v4() }),
+  });
+};
 
-listaClientes().then((data)=>{
-    data.forEach((perfil)=>{
-        const nuevaLinea=crearNuevaLinea(perfil.nombre,perfil.email);
-        table.appendChild(nuevaLinea);
-    });
-})
-.catch((error) => alert("Ocurrió un error"));
+const eliminarCliente = (id) => {
+  return fetch(`http://localhost:3000/perfil/${id}`, {
+    method: "DELETE",
+  });
+};
 
-
+export const clientServices = {
+  listaClientes,
+  crearCliente,
+  eliminarCliente,
+};
