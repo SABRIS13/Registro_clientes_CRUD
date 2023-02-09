f3efc361b7ccba92924e0875afa16e9f8335183e
Sabrina Contreras <sabriscon13@hotmail.com>
2023-02-09 15:05:20 -0700

    Funcionalidad de función async y catch

--- controllers/actualizar.controller.js
+++ controllers/actualizar.controller.js
@@ -2,22 +2,29 @@ import { clientServices } from "../service/client-service.js";
 
 const formulario = document.querySelector("[data-form]");
 
-const obtenerInformacion = () => {
-  const url = new URL(window.location);
-  const id = url.searchParams.get("id");
-
-  if (id === null) {
-    window.location.href = "/screens/error.html";
-  }
-
-  const nombre = document.querySelector("[data-nombre]");
-  const email = document.querySelector("[data-email]");
-
-  clientServices.detalleCliente(id).then((perfil) => {
-    nombre.value = perfil.nombre;
-    email.value = perfil.email;
-  });
-};
+const obtenerInformacion = async () => {
+    const url = new URL(window.location);
+    const id = url.searchParams.get("id");
+  
+    if (id === null) {
+      window.location.href = "/screens/error.html";
+    }
+  
+    const nombre = document.querySelector("[data-nombre]");
+    const email = document.querySelector("[data-email]");
+  
+    try {
+      const perfil = await clientServices.detalleCliente(id);
+      if (perfil.nombre && perfil.email) {
+        nombre.value = perfil.nombre;
+        email.value = perfil.email;
+      } else {
+        throw new Error();
+      }
+    } catch (error) {
+      window.location.href = "/screens/error.html";
+    }
+  };
 
 obtenerInformacion();
 
